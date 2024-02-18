6f3f40686c4fbc2f9e4de0d4a95fa3287f66bfff
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-02-18 09:17:12 +0530

    create login page

---
+++ FBLogin/src/component/login.tsx
@@ -0,0 +1,12 @@
+import './login.css'
+import  "bootstrap/dist/css/bootstrap.min.css"
+
+function Login(){
+    return(
+        <>
+        <h1>Hi</h1>
+
+        </>
+    );
+};
+export default Login
