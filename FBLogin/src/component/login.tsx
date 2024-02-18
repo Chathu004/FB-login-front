bc1ccb727f504041f7a02ed9fa9953eb5a99e403
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-02-18 09:54:45 +0530

    create login page frontend

--- FBLogin/src/component/login.tsx
+++ FBLogin/src/component/login.tsx
@@ -1,12 +1,27 @@
 import './login.css'
-import  "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function Login(){
-    return(
+function Login() {
+    return (
         <>
-        <h1>Hi</h1>
-
+            <div className="content">
+                <div className="flex-div">
+                    <div className="name-content">
+                        <h1 className="logo">Facebook</h1>
+                        <p>Connect with friends and the world around you on Facebook.</p>
+                    </div>
+                    <form>
+                        <input type="text" placeholder="Email or Phone Number" required />
+                        <input type="password" placeholder="Password" required />
+                        <button className="login">Log In</button>
+                        <a href="#">Forgot Password ?</a>
+                        <hr />
+                        <button className="create-account">Create New Account</button>
+                    </form>
+                </div>
+            </div>
         </>
     );
 };
+
 export default Login
