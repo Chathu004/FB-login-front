2bfadcee792018e2c7a147a6e68cbe720f8ae503
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-03-10 08:48:09 +0530

    update yml

--- FBLogin/src/component/login.tsx
+++ FBLogin/src/component/login.tsx
@@ -4,22 +4,32 @@ import "bootstrap/dist/css/bootstrap.min.css"
 function Login() {
     return (
         <>
-            <div className="content">
-                <div className="flex-div">
-                    <div className="name-content">
-                        <h1 className="logo">Facebook</h1>
+            <div className="container">
+
+                {/* <div className="content"> */}
+                <div className=" justify-content-center row">
+
+                    <div className="col-lg-6 col-sm-12">
+                        <h1 className="logo">facebook</h1>
                         <p>Connect with friends and the world around you on Facebook.</p>
                     </div>
-                    <form>
-                        <input type="text" placeholder="Email or Phone Number" required />
-                        <input type="password" placeholder="Password" required />
-                        <button className="login">Log In</button>
-                        <a href="#">Forgot Password ?</a>
-                        <hr />
-                        <button className="create-account">Create New Account</button>
-                    </form>
+                    <div className="col-lg-6 col-sm-12">
+                        <form>
+                            <input type="text" placeholder="Email or Phone Number" required />
+                            <input type="password" placeholder="Password" required />
+                            <button className="login">Log In</button>
+                            <a href="#">Forgot Password ?</a>
+                            <hr />
+                            <button className="create-account">Create New Account</button>
+                        </form>
+                    </div>
+
                 </div>
+
             </div>
+            {/* </div> */}
+
+
         </>
     );
 };
